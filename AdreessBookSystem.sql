513f32df73644d55cd177743f32baeee1ce5004f
Rahulgautama <rahul8874950727@gmail.com>
2023-06-10 03:32:05 +0530

    create Table addresbook using Sql

--- AdreessBookSystem.sql
+++ AdreessBookSystem.sql
@@ -1,1 +1,1 @@
-create database AddressBook_Service
+create Table Address_Book(FirstName varchar(30),LastName varchar(30),Address varchar(50),City varchar(30),State varchar(30),Zip int,PhoneNumber int,Email varchar(45));
