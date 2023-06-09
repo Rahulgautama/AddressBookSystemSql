3e82c85df17052bb4d51403120ad03330fc686c8
Rahulgautama <rahul8874950727@gmail.com>
2023-06-10 04:21:14 +0530

    Select By Type and name contact Data addresbook using Sql

--- AdreessBookSystem.sql
+++ AdreessBookSystem.sql
@@ -1,6 +1,2 @@
-alter table Address_Book add Name varchar(30),Type varchar(20)
-
-update Address_Book Set type='Family',Name='SmithJohn' where FirstName='Smith'
-
-select * from Address_Book where type='Family' and Name='SmithJohn'
+Select Count(Type) from Address_Book where PhoneNumber=7984777382;
 
