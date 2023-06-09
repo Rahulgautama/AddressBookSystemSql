2e718bc7b8c671a6ea0ae07a37e7d4794969eac1
Rahulgautama <rahul8874950727@gmail.com>
2023-06-10 04:14:03 +0530

    Select By Type and name contact Data addresbook using Sql

--- AdreessBookSystem.sql
+++ AdreessBookSystem.sql
@@ -1,1 +1,6 @@
-Select FirstName  from Address_Book order by City
+alter table Address_Book add Name varchar(30),Type varchar(20)
+
+update Address_Book Set type='Family',Name='SmithJohn' where FirstName='Smith'
+
+select * from Address_Book where type='Family' and Name='SmithJohn'
+
