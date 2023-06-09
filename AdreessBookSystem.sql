c8347361f934dbb6efe33bf8db68e9030d1c345a
Rahulgautama <rahul8874950727@gmail.com>
2023-06-10 03:45:32 +0530

    Insert into new contact addresbook using Sql

--- AdreessBookSystem.sql
+++ AdreessBookSystem.sql
@@ -1,1 +1,1 @@
-create Table Address_Book(FirstName varchar(30),LastName varchar(30),Address varchar(50),City varchar(30),State varchar(30),Zip int,PhoneNumber int,Email varchar(45));
+Insert into Address_Book(FirstName,LastName,Address,City,State,Zip,PhoneNumber,Email) values('Bill','Kumar','Lucknow','Kanpur','UP',12773,9388477783,'Bl12@gmail.com')
